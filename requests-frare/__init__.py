50b78a16b1a04e60824a8cca3fcfb8f6dcfc78aa
zearakun <91083604+zearakun@users.noreply.github.com>
2022-07-02 22:38:17 +0900

    Update __init__.py

--- requests-frare/__init__.py
+++ requests-frare/__init__.py
@@ -1,5 +1,4 @@
-import cloudscraper
-scraper = cloudscraper.create_scraper()
+import requests
 import discord
 from discord import commands
 
@@ -8,7 +7,12 @@ from discord import commands
 class selfBot():
   async def tools(token):
     global token
+    header = {"authorization": token}
+    data = requests.get("https://discord.com/api/v9/users/@me",headers=header).json()
+    print(data["username"] + data["discriminator"] + " : login")
     def send(content,channel):
       headers = {"authorization": token}
       threading.Thread(target=requests.post(f"https://discord.com/api/v9/channels/{channel}/messages",data={"content": content},headers=headers)).start
     def typing(channenl):
+      headers = {"authorization": token}
+      threading.Thread(target=requests.get(f"https://discord.com/api/v9/channels/{channel}/typing",headers=headers)).start
